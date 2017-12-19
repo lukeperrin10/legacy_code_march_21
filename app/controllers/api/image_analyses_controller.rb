2d5db6a9bc28a2cfcf451881c0982e10a17a8c99
tochman <thomas@craftacademy.se>
2017-12-19 17:08:52 +0100

    configures clarifai wrapper with credentials

--- app/controllers/api/image_analyses_controller.rb
+++ app/controllers/api/image_analyses_controller.rb
@@ -2,5 +2,18 @@ class Api::ImageAnalysesController < ApplicationController
 
   def create
     binding.pry
+    # sanitize the params
+
+    # hit the clarifai api
+
+    # save the results in db
+
+    # render a response to client
+  end
+
+  private
+
+  def analysis_params
+    params.require(:analysis).permit!
   end
 end
--- spec/request/image_analyses_spec.rb
+++ spec/request/image_analyses_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Api::ImageAnalysesController, type: :request do
 
   it 'let us have a test' do
-    post '/api/image_analyses', params: { image_analysis: {random: :param}}
+    post '/api/image_analyses', params: { analysis: {random: :param}}
     binding.pry
   end
 end
