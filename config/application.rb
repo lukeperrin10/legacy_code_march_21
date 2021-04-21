2f5ff79b495b266555127e9ed995ab416b66da05
luke <lukeperrin27@gmail.com>
2021-04-21 21:24:18 +0200

    reverts back to first main version after config errors updated with new api keys and added application generators

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,7 @@
-# frozen_string_literal: true
 
 require_relative 'boot'
 
 require 'rails'
-# Pick the frameworks you want:
 require 'active_model/railtie'
 require 'active_job/railtie'
 require 'active_record/railtie'
@@ -11,26 +9,24 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'action_cable/engine'
-# require "sprockets/railtie"
 require 'rails/test_unit/railtie'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module MyAwesomeApi
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators  do |generate|
+      generate.helper false
+      generate.assets false
+      generate.skip_routes true
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+      generate.request_specs false
+    end
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
